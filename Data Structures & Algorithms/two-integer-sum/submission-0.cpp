d01ec46d38380c75259616e94931f215c1fd1730
Manthan Marathe <118454601+ManthanMarathe223@users.noreply.github.com>
2026-06-02 20:36:00 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& n, int t) {
+        
+        for(int i=0; i<(n.size()-1); i++){
+            for(int j=i+1; j<n.size(); j++){
+
+                if(n[i]+n[j] == t){
+                    return {i, j};
+                }
+            }
+        }
+    }
+};
